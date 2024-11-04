0a240d9a3b361d88dbf758bba4297c80b5753f27
Tytianiuk Artem <tytianiuk@gmail.com>
2024-11-04 13:59:52 +0200

    feat: implement category controller
    - implement get, post, delete methods of category
    -fix category routes

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,7 +1,30 @@
 import { data } from '../data.js';
 
-export const createCategory = (req, res) => {};
+export const createCategory = (req, res) => {
+  const { id, name } = req.body;
+  data.categories.push({ id, name });
+  res
+    .status(201)
+    .json({ message: 'Category is created', category: { id, name } });
+};
 
-export const deleteCategory = (req, res) => {};
+export const deleteCategory = (req, res) => {
+  const index = data.categories.findIndex(
+    (category) => category.id === req.params.category_id,
+  );
+  if (index !== -1) {
+    data.categories.splice(index, 1);
+    res.json({ message: 'Category is deleted' });
+  } else {
+    res.status(404).json({ message: 'Category is not found' });
+  }
+};
 
-export const getAllCategory = (req, res) => {};
+export const getCategory = (req, res) => {
+  const category = data.categories.find(
+    (category) => category.id === req.params.category_id,
+  );
+  category
+    ? res.json(category)
+    : res.status(404).json({ message: 'Category is not found' });
+};
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,13 +2,13 @@ import express from 'express';
 import {
   createCategory,
   deleteCategory,
-  getAllCategory,
+  getCategory,
 } from '../controllers/categoryController.js';
 
 const router = express.Router();
 
 router.post('/', createCategory);
-router.delete('/', deleteCategory);
-router.get('/', getAllCategory);
+router.delete('/:category_id', deleteCategory);
+router.get('/:category_id', getCategory);
 
 export default router;
