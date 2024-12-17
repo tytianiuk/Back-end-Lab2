78550d4f41ee4145b6a5a58ac27499e801c93471
Tytianiuk Artem <tytianiuk@gmail.com>
2024-12-18 01:13:05 +0200

    feat: install mongoDB and configure docker-compose

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ import userRoutes from './routes/userRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import recordRoutes from './routes/recordRoutes.js';
 import { getAllUsers } from './controllers/userController.js';
+import connectDatabase from './db.js';
 
 const server = express();
 const PORT = 3000;
+
+connectDatabase();
 
 server.use(express.json());
 
